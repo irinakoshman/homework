974ba0b188f51e2e04046982e518864830e056ce
Irina Koshman <koshman.irina@yandex.ru>
2022-12-19 12:33:00 +0300

    задача №8

---
+++ Homework_001_ex8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Number = 2;
+
+while (Number < N)
+{
+    Console.WriteLine(Number);
+    Number +=2;
+}
