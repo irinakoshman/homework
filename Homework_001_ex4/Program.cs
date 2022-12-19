0d8688721f558fea4abea9e1625b7f214bf01121
Irina Koshman <koshman.irina@yandex.ru>
2022-12-19 11:59:19 +0300

    задача №4

---
+++ Homework_001_ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите значение A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение B");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение C");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+{
+    if (B> max) max = B;
+    if (C> max) max = C;
+}
+Console.WriteLine($"Максимальное из введенных значений {max}");
