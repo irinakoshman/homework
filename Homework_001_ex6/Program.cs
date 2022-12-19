dd12139a5d4d3b429d00cf62b86c7ea3d6e9c102
Irina Koshman <koshman.irina@yandex.ru>
2022-12-19 12:13:39 +0300

    задача №6

---
+++ Homework_001_ex6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число : N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+    Console.WriteLine($"Число {N} четное");
+}
+else
+{
+    Console.WriteLine($"Число {N} нечетное");
+}
