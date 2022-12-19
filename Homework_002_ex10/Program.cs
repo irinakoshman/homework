318e7c8343ab70589c856d88f5f68a2d68352b8f
Irina Koshman <koshman.irina@yandex.ru>
2022-12-19 21:30:03 +0300

    задача 10

---
+++ Homework_002_ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+Console.WriteLine("Введите 3х значное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1000 && number > 99) Console.WriteLine("Задача выполняется");
+else Console.WriteLine("Повторите попытку ввода");
+
+int number2 = number%100;
+int number3 = number2/10;
+Console.WriteLine($"{number3}");
+
