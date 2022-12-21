1bd023d4c9b703303a98d51a236cd9f19f77282c
Irina Koshman <koshman.irina@yandex.ru>
2022-12-21 14:06:01 +0300

    задача 13

---
+++ Homework_002_ex13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, 
+//которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number/100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+string strnumber3 = Convert.ToString(number);
+Console.Write(strnumber3[2]);
+}
