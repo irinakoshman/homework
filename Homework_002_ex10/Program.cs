bd08bb3c1cde49b8d518668d0465f0db37f9ea96
Irina Koshman <koshman.irina@yandex.ru>
2022-12-26 16:56:29 +0300

    задача 10

--- Homework_002_ex10/Program.cs
+++ Homework_002_ex10/Program.cs
@@ -8,7 +8,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (number < 1000 && number > 99) Console.WriteLine("Задача выполняется");
 else Console.WriteLine("Повторите попытку ввода");
 
-int number2 = number%100;
-int number3 = number2/10;
+int number2 = number % 100;
+int number3 = number2 / 10;
 Console.WriteLine($"{number3}");
 
