aecd160e0226ef01c6e00f54a63a81c86ba94837
Irina Koshman <koshman.irina@yandex.ru>
2023-01-15 12:41:49 +0300

    задача 27

---
+++ Homework_004_ex27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summa = Sum(number);
+Console.WriteLine($"Сумма цифр в числе {number} : {summa} ");
